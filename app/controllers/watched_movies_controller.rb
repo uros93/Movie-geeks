243c15b3bede37971c2731fe419f9bf9b80906b5
Uros <uroscertic@gmail.com>
2016-07-11 19:32:08 +0000

    fix2

--- app/controllers/watched_movies_controller.rb
+++ app/controllers/watched_movies_controller.rb
@@ -1,5 +1,6 @@
 class WatchedMoviesController < ApplicationController
     def create
+        
         unless Movie.exists?(:imdbid => params[:imdbid])
             movie = Imdb::Movie.new(params[:imdbid])
             add = Movie.new(:title => movie.title, :imdbid => movie.id, :poster => movie.poster)
@@ -10,11 +11,17 @@ class WatchedMoviesController < ApplicationController
             end
             watched = current_user.watched_movies.build(:movie_id => add.id)
         else
+            
             result = Movie.where(:imdbid => params[:imdbid]).first
-            watched = current_user.watched_movies.build(:movie_id => result.id)
+            if current_user.movies.include?(result)
+                redirect_to user_path(current_user)
+                return
+            else
+                watched = current_user.watched_movies.build(:movie_id => result.id)
+            end
         end
             if watched.save
-                redirect_to root_path
+                redirect_to user_path(current_user)
             else
             end
     end
